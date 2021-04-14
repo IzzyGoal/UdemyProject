27d355002163948befefab674baaa831dcb22727
Gabriel Silva Lima <gabriel.osilvalima@gmail.com>
2021-04-14 16:53:40 -0300

    First Commit

---
+++ src/main/java/com/silvagabriel/www/CursoproApplication.java
@@ -0,0 +1,13 @@
+package com.silvagabriel.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silvagabriel/www/CursoproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvagabriel.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
